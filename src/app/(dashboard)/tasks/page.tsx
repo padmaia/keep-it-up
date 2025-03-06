653c08c658130d3f11c442a943c8182fca17a5bd
Maia Teegarden <dev@padmaia.rocks>
2025-03-05 21:18:29 -0700

    force dynamic for more pages

--- src/app/(dashboard)/tasks/page.tsx
+++ src/app/(dashboard)/tasks/page.tsx
@@ -11,6 +11,8 @@ import TaskList from "@/components/TaskList"
 //   history: string[]
 // }
 
+export const dynamic = 'force-dynamic'
+
 export default async function TasksPage() {
   const tasks = await getTasks()
 
